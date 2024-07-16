b33245dfa988739e43509a314efdc030c4126a9b
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-07-16 20:57:22 +0400

    simple review slider

---
+++ ReviewSlide/app.js
@@ -0,0 +1,67 @@
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://www.course-api.com/images/people/person-1.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://www.course-api.com/images/people/person-2.jpeg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+const left = document.querySelector(".prev-btn");
+const right = document.querySelector(".next-btn");
+const surprise = document.querySelector(".random-btn");
+let revName = document.querySelector("#author");
+let revJob = document.querySelector("#job");
+let image = document.querySelector("#person-img");
+let info = document.querySelector("#info");
+let currentIndex = 0;
+
+right.addEventListener("click", () => {
+  if (currentIndex < reviews.length-1) {
+    currentIndex += 1;
+    image.src = reviews[currentIndex].img;
+    revName.textContent = reviews[currentIndex].name;
+    revJob.textContent = reviews[currentIndex].job;
+    info.textContent = reviews[currentIndex].text;
+  }
+});
+
+left.addEventListener("click", () => {
+  if (currentIndex > 0 && currentIndex < reviews.length) {
+    currentIndex -= 1;
+    image.src = reviews[currentIndex].img;
+    revName.textContent = reviews[currentIndex].name;
+    revJob.textContent = reviews[currentIndex].job;
+    info.textContent = reviews[currentIndex].text;
+  }
+});
+
+surprise.addEventListener('click',function(){
+  currentIndex = Math.floor(Math.random()*4)
+  image.src = reviews[currentIndex].img;
+    revName.textContent = reviews[currentIndex].name;
+    revJob.textContent = reviews[currentIndex].job;
+    info.textContent = reviews[currentIndex].text;
+})
