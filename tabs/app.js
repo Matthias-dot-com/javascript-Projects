2cd3196e235a77222e815c74632bfeb7819ef8e3
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-07-18 00:08:00 +0400

    all functionality added

--- tabs/app.js
+++ tabs/app.js
@@ -1,1 +1,19 @@
-const 
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+const about = document.querySelector(".about");
+
+about.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    articles.forEach((article) => {
+      article.classList.remove("active");
+    });
+
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
